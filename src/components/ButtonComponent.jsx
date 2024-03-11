20442a0025d9b4dbd842914365084545c1c1b053
Nico_le <156796855+NicoleMaini@users.noreply.github.com>
2024-03-11 17:48:25 +0100

    another script

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -1,8 +1,10 @@
 // un componente react può semplicemente essere una funzione che ritorna un file .jsx
 
-const ButtonComponent = props => {
+// const ButtonComponent = props => {
+const ButtonComponent = ({ bgBtn, innerPhrase }) => {
   return (
-    <button style={props.bgBtn}>{props.innerPhrase || "clicca qui"} </button>
+    <button style={bgBtn}>{innerPhrase || "clicca qui"} </button>
+    // <button style={props.bgBtn}{{props.innerPhrase || "clicca qui"} </button>
     // non si posso ritornare due elemneti da un componente, ma si possono racchiudere più elementi in uno - div dentro un div con dentro altri tag
     // <div>
     //     <h3>Non si può fare</h3>
--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -2,9 +2,12 @@ import { Component } from "react";
 
 class ImageComponent extends Component {
   render() {
+    const { link, name } = this.props;
+
     return (
       <div className="container">
-        <img src={this.props.link} alt={this.props.name} />
+        {/* <img src={this.props.link} alt={this.props.name} /> */}
+        <img src={link} alt={name} />
       </div>
     );
   }
